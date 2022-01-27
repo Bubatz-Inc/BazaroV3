321da027df3358468462f91047607cfe69bc261a
R4cc <48733309+R4cc@users.noreply.github.com>
2022-01-27 15:47:41 +0100

    New sidebar (WIP) (#45)
    * Auto stash before merge of "new-sidebar" and "origin/main"
    
    * Fix query folderid bug
    
    * Update GetViewEntriesByFolderId
    
    * Sidebar dark mode
    
    * added create rootFolder on register
    
    * Fix include in subfolder req
    
    Co-authored-by: Niklas L <niklas.lerchenmueller@protonmail.com>
    Co-authored-by: Z3r0z0 <tobi.as.1@gmx.at>

--- Bazaro.Web/Services/Queries/Folders/GetViewFolderStructureByUserId.cs
+++ Bazaro.Web/Services/Queries/Folders/GetViewFolderStructureByUserId.cs
@@ -12,20 +12,16 @@ namespace Bazaro.Web.Services.Queries.Folders
             public string UserId { get; set; }
         }
 
-        public static async Task<List<FolderModel>> Handle(BazaroContext context, Query request)
+        public static async Task<FolderModel> Handle(BazaroContext context, Query request)
         {
-            var topFolders = await context.Set<UserFolderReference>()
+            var rootFolder = await context.Set<UserFolderReference>()
+                .Include(x => x.Folder)
+                .ThenInclude(x => x.SubFolder)
                 .Where(x => x.UserId == request.UserId && (x.Folder.PreviousFolder == null || x.IsShared))
                 .Select(x => x.Folder)
-                .ToListAsync();
+                .FirstOrDefaultAsync();
 
-            var result = new List<FolderModel>();
-            foreach (var folder in topFolders)
-            {
-                result.Add(CreateFolderStructure(folder));
-            }
-
-            return result;
+            return CreateFolderStructure(rootFolder);
         }
 
         private static FolderModel CreateFolderStructure(Folder folder)
