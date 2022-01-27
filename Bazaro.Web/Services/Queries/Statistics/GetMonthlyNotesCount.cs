5472cc361294eaf20d91ebd37545ec3ea795e5ee
Z3r0z0 <66936503+Z3r0z0@users.noreply.github.com>
2022-01-27 09:16:35 +0100

    add statistics backend (#37)
    * update
    
    * Added service to DI

---
+++ Bazaro.Web/Services/Queries/Statistics/GetMonthlyNotesCount.cs
@@ -0,0 +1,37 @@
+﻿using Bazaro.Web.Models.References;
+using Bazaro.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using Bazaro.Web.Services.ViewModels;
+
+namespace Bazaro.Web.Services.Queries.Statistics
+{
+    public static class GetMonthlyNotesCount
+    {
+        public class Query
+        {
+            public string UserId { get; set; }
+        }
+
+        public static async Task<List<MonthlyStatViewModel>> Handle(BazaroContext context, Query request)
+        {
+            var folders = await context.Set<UserFolderReference>().Where(x => x.UserId == request.UserId).ToListAsync();
+
+            if(folders == null)
+                return null;
+
+            List<Entry> entries = new List<Entry>();
+            foreach(var folder in folders)
+            {
+                entries.AddRange(await context.Set<FolderEntryReference>().Where(x => x.FolderId == folder.FolderId).Select(x => x.Entry).ToListAsync());
+            }
+
+            return entries.GroupBy(x => new { x.Created.Month, x.Created.Year })
+                .Select(x => new MonthlyStatViewModel
+                {
+                    Year = x.Key.Year,
+                    Month = x.Key.Month,
+                    Count = x.Count()
+                }).ToList();
+        }
+    }
+}
---
+++ Bazaro.Web/Services/StatisticService.cs
@@ -0,0 +1,17 @@
+﻿using Bazaro.Web.Services.Queries.Statistics;
+using Bazaro.Web.Services.ViewModels;
+
+namespace Bazaro.Web.Services
+{
+    public class StatisticService
+    {
+        private readonly BazaroContext _context;
+
+        public StatisticService(BazaroContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<MonthlyStatViewModel>> GetMonthlyStatsByUserId(string userId) => GetMonthlyNotesCount.Handle(_context, new GetMonthlyNotesCount.Query { UserId = userId });
+    }
+}
