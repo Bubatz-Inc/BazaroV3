3dd5e28145d36449872e57b8ebfd927667ea4fd1
Z3r0z0 <tobi.as.1@gmx.at>
2022-01-27 15:23:16 +0100

    Added TestFramework

---
+++ Bazaro.Test.Base/DatabaseBaseFixture.cs
@@ -0,0 +1,57 @@
+﻿using Bazaro.Web;
+using Bazaro.Web.Services;
+using Microsoft.EntityFrameworkCore;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using System;
+
+namespace Bazaro.Test.Base
+{
+    public class DatabaseBaseFixture : IDisposable
+    {
+        internal readonly Container _container;
+
+        private bool _databaseOnTimeCalled = false;
+
+        public DatabaseBaseFixture()
+        {
+
+        }
+
+        private Container BildContainer()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            var builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+            builder.ConfigureWarnings(w => w.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.InMemoryEventId.TransactionIgnoredWarning));
+
+            container.RegisterInstance(builder.Options);
+            container.Register<BazaroContext, BazaroContext>(Lifestyle.Scoped);
+
+            container.Register<EntryService>(Lifestyle.Transient);
+            container.Register<FolderService>(Lifestyle.Transient);
+            container.Register<ItemService>(Lifestyle.Transient);
+            container.Register<EntryRelationService>(Lifestyle.Transient);
+            container.Register<StatisticService>(Lifestyle.Transient);
+            container.Register<ContentTypeSevice>(Lifestyle.Transient);
+
+            return container;
+        }
+
+        internal void CreateDatabaseDataOneTime(Action fillMethod)
+        {
+            if (_databaseOnTimeCalled)
+                return;
+
+            _databaseOnTimeCalled = true;
+            fillMethod.Invoke();
+        }
+
+        public void Dispose()
+        {
+            _container.Dispose();
+        }
+    }
+}
---
+++ Bazaro.Test.Base/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using Bazaro.Web;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using Xunit;
+
+namespace Bazaro.Test.Base
+{
+    [Collection(nameof(TestBase))]
+    public abstract class TestBase
+    {
+        protected readonly Scope _scope;
+        protected readonly BazaroContext _context;
+
+        private readonly DatabaseBaseFixture _fixture;
+
+        public TestBase(DatabaseBaseFixture fixture)
+        {
+            _fixture = fixture;
+
+            _scope = AsyncScopedLifestyle.BeginScope(fixture._container);
+            _context = _scope.GetInstance<BazaroContext>();
+        }
+
+        protected void CreateDatabaseOnTimeData() => _fixture.CreateDatabaseDataOneTime(() => DatabaseOneTimeData());
+
+        protected virtual void DatabaseOneTimeData()
+        {
+
+        }
+    }
+}
