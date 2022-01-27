6f5bfdb3781f4f37534138e494e9a4e13dacaa87
Z3r0z0 <tobi.as.1@gmx.at>
2022-01-27 14:01:29 +0100

    major database update and added service for contentType

---
+++ Bazaro.Web/Services/Commands/ContentTypes/InsertContentType.cs
@@ -0,0 +1,23 @@
+﻿using Bazaro.Web.Models;
+
+namespace Bazaro.Web.Services.Commands.ContentTypes
+{
+    public static class InsertContentType
+    {
+        public class Command
+        {
+            public string Title { get; set; }
+        }
+
+        public static Task Handle(BazaroContext context, Command request)
+        {
+            context.Add(new ContentType
+            {
+                Title = request.Title,
+                Created = DateTime.Now
+            });
+
+            return context.SaveChangesAsync();  
+        }
+    }
+}
---
+++ Bazaro.Web/Services/ContentTypeSevice.cs
@@ -0,0 +1,22 @@
+﻿using Bazaro.Web.Services.Commands.ContentTypes;
+using Bazaro.Web.Services.Queries.ContentTypes;
+using Bazaro.Web.Services.ViewModels;
+
+namespace Bazaro.Web.Services
+{
+    public class ContentTypeSevice
+    {
+        public readonly BazaroContext _context;
+
+        public ContentTypeSevice(BazaroContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<ContentTypeModel>> GetContentTypes(GetContentTypes.Query request) => Queries.ContentTypes.GetContentTypes.Handle(_context, request);
+
+        public Task Insert(InsertContentType.Command request) => InsertContentType.Handle(_context, request);
+        public Task Update(UpdateContentType.Command request) => UpdateContentType.Handle(_context, request);
+        public Task Delete(DeleteContentType.Command request) => DeleteContentType.Handle(_context, request);
+    }
+}
