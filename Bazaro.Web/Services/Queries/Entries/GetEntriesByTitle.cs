fb5f735dd1469ad62c1313db59e768e6e20cf075
Jonas <jonas@jmp.blue>
2022-02-01 07:52:19 +0000

    Calendar create / update / delete entries (#60)
    * add note select
    
    * load existing entries
    
    * fix seeing other users notes
    
    * update / delete calendar entries

--- Bazaro.Web/Services/Queries/Entries/GetEntriesByTitle.cs
+++ Bazaro.Web/Services/Queries/Entries/GetEntriesByTitle.cs
@@ -1,4 +1,5 @@
 ﻿using Bazaro.Web.Models;
+using Bazaro.Web.Models.References;
 using Bazaro.Web.Services.ViewModels;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,7 @@ namespace Bazaro.Web.Services.Queries.Entries
         public class Query
         {
             public string Title { get; set; }
+            public string UserId { get; set; }
             public int MaxCount { get; set; }
         }
 
@@ -17,6 +19,16 @@ namespace Bazaro.Web.Services.Queries.Entries
             return context.Set<Entry>()
                 .Where(x => x.Title.ToLower().Contains(request.Title.ToLower())
                     || request.Title.ToLower().Contains(x.Title))
+                .Join(context.Set<FolderEntryReference>(),
+                    e => e.Id,
+                    fe => fe.EntryId,
+                    (e, fe) => new { e, fe})
+                .Join(context.Set<UserFolderReference>(),
+                    efe => efe.fe.FolderId,
+                    uf => uf.FolderId,
+                    (efe, uf) => new { efe, uf })
+                .Where(x => x.uf.UserId == request.UserId)
+                .Select( x => x.efe.e)
                 .Take(request.MaxCount)
                 .Select(x => new EntryModel
                 {
