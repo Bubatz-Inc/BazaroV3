9ad4d8f997060fd8e1e015e4da477caa4d6fa797
Z3r0z0 <tobi.as.1@gmx.at>
2022-02-02 09:15:21 +0100

    Update

--- Bazaro.Core.Test/Commands/EntryRefernceTest.cs
+++ Bazaro.Core.Test/Commands/EntryRefernceTest.cs
@@ -1,16 +1,16 @@
 ﻿using Bazaro.Test.Base;
 using Bazaro.Web.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Bazaro.Core.Test.Commands
 {
     public class EntryRefernceTest : TestBase, IClassFixture<DatabaseBaseFixture>
     {
-        private readonly EntryRelationService _service
+        private readonly EntryRelationService _service;
+
+        public EntryRefernceTest(DatabaseBaseFixture fixture) : base(fixture)
+        {
+
+        }
     }
 }
