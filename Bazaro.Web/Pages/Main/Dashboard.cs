036fbcd452665916f4b3fab3cbdad60fb5bb9a75
Niklas L <niklas.lerchenmueller@protonmail.com>
2022-02-02 11:00:21 +0100

    Add recent notes

--- Bazaro.Web/Pages/Main/Dashboard.cs
+++ Bazaro.Web/Pages/Main/Dashboard.cs
@@ -1,5 +1,8 @@
-﻿using Bazaro.Web.Services.ViewModels;
+﻿using Bazaro.Web.Models;
+using Bazaro.Web.Services;
+using Bazaro.Web.Services.ViewModels;
 using Blazorise.Charts;
+using System.Text;
 
 namespace Bazaro.Web.Pages.Main
 {
@@ -9,14 +12,20 @@ namespace Bazaro.Web.Pages.Main
 
         protected override async Task OnInitializedAsync()
         {
+            await Load5LastNotes();
             await LoadThisWeeksEvents();
         }
 
         // Recently Opened
         private async Task Load5LastNotes()
         {
+            string userId = await GetCurrentUserId();
 
-            // Load _last5Entries with entryService
+            _last5Entries = (await statisticsService.GetRecentlyUpdatedEntries(new Services.Queries.Statistics.GetRecentEntriesByUserId.Query
+            {
+                UserId = userId,
+                MaxCount = 10,
+            }));
         }
 
         private List<CalendarEntryModel> _calendarEntries { get; set; } = new();
@@ -25,8 +34,7 @@ namespace Bazaro.Web.Pages.Main
         private async Task LoadThisWeeksEvents()
         {
             var firstDay = new DateTime(PanelDate.Year, PanelDate.Month, 1);
-            var username = httpContext.HttpContext.User.Identity.Name;
-            string userId = (await userManager.FindByNameAsync(username)).Id;
+            string userId = await GetCurrentUserId();
             if (userId is null) return;
             _calendarEntries = (await calendarService.GetCalendarEntries(new Services.Queries.CalendarEntries.GetCalenderEntriesByUserId.Query
             {
@@ -39,5 +47,40 @@ namespace Bazaro.Web.Pages.Main
             StateHasChanged();
         }
 
+        private async Task<string> GetCurrentUserId()
+        {
+            var username = httpContext.HttpContext.User.Identity.Name;
+            return (await userManager.FindByNameAsync(username)).Id;
+        }
+
+        private async Task OpenEntry(int entryId)
+        {
+            Nav.NavigateTo($"/noteeditor/{entryId}");
+        }
+
+        private async Task CreateNewQucikNote(string content)
+        {
+            var userId = await GetCurrentUserId();
+
+            var rootfolder = await folderService.GetViewFolderStructureByUserId(new Services.Queries.Folders.GetViewSubfoldersStructureByUserId.Query
+            {
+                UserId = userId
+            });
+
+            await entryService.Insert(new Services.Commands.Entries.InsertEntry.Command
+            {
+                FolderId = rootfolder.Id,
+                Title = "New Quicknote " + DateOnly.FromDateTime(DateTime.Now),
+            });
+
+            //await itemService.Insert(new Services.Commands.Items.InsertItem.Command
+            //{
+            //    ContentTypeId = 1,
+            //    Content = Encoding.UTF8.GetBytes(TextAreaValue),
+            //    EntryId = EntryId
+            //}
+            //); 
+        
+        }
     }
 }
