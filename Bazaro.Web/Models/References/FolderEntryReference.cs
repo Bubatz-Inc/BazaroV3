eb11cf3737b7cf1848e9b075da52acb133a1dda4
Z3r0z0 <tobi.as.1@gmx.at>
2022-02-03 11:19:35 +0100

    Updated FolderEntryReference

--- Bazaro.Web/Models/References/FolderEntryReference.cs
+++ Bazaro.Web/Models/References/FolderEntryReference.cs
@@ -5,9 +5,9 @@ namespace Bazaro.Web.Models.References
     public record class FolderEntryReference : IdEntity
     {
         public Folder Folder { get; set; }
-        public int? FolderId { get; set; }
+        public int FolderId { get; set; }
 
         public Entry Entry { get; set; }
-        public int? EntryId { get; set; }
+        public int EntryId { get; set; }
     }
 }
