f34c9c46b04d1a807b15b8388b9826808a01ee74
Z3r0z0 <tobi.as.1@gmx.at>
2022-02-03 08:41:01 +0100

    Added FolderTest

---
+++ Bazaro.Core.Test/Commands/FolderTest.cs
@@ -0,0 +1,26 @@
+﻿using Bazaro.Test.Base;
+using Bazaro.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bazaro.Core.Test.Commands
+{
+    public class FolderTest : TestBase, IClassFixture<DatabaseBaseFixture>
+    {
+        private readonly FolderService _service;
+
+        public FolderTest(DatabaseBaseFixture fixture) : base(fixture)
+        {
+            _service = _scope.GetInstance<FolderService>();
+        }
+
+        protected override void DatabaseOneTimeData()
+        {
+            base.DatabaseOneTimeData();
+        }
+    }
+}
